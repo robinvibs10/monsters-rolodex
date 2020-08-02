ba8caeb2238601ce2ab5481bc150ecf314c16b5d
Your Namerobinvibs10 git config --global user.name Your <robinvibs10@gmail.com>
2020-08-02 19:56:08 +0800

    Updates

---
+++ precache-manifest.47bfd722cf976bc6b7e2fa714130b3f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5261e0585036ad09bfe4e342e36cb77f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ea2f11efb101948576b",
+    "url": "/monsters-rolodex/static/css/main.5337eb6d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea2f11efb101948576b",
+    "url": "/monsters-rolodex/static/js/main.082973f7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
